d313a9f9774c1854649a9521088887c0ec67877f
cuining <cncc0824@gmail.com>
2019-12-31 17:41:07 +0800

    remove

--- dist/cache.d.ts
+++
@@ -1,8 +0,0 @@
-export default class Cache<T extends object> {
-  map: WeakMap<T, number>
-  constructor()
-  get(key: T): number
-  has(key: T): boolean
-  set(key: T, value: any): void
-  reset(): void
-}
